4757146d10416af9b17de734f3a9901ff3e18d95
V-Khvorostianyi <slavafh@gmail.com>
2017-12-25 10:37:56 +0200

    add some elemets with kv

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-import kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.layout import Layout
+from kivy.properties import ObjectProperty
+
+class Root(Widget):
+    pass
+
+class InputForm(Layout):
+    pass
+    form = ObjectProperty(None)
+
+class SimpleApp(App):
+    def build(self):
+        return Root()
+
+if __name__ == '__main__':
+    SimpleApp().run()

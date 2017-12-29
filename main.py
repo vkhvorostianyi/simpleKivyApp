1a8523f9dc36181c5b4fcad6d82f75b5b42c2985
V-Khvorostianyi <slavafh@gmail.com>
2017-12-29 20:38:20 +0200

    add grid

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.uix.layout import Layout
-from kivy.properties import ObjectProperty
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from wallet_app import *
 
 
 class Root(Widget):
-    pass
 
-class InputForm(Layout):
-    pass
-    form = ObjectProperty(None)
+    def layout(self):
+        layout = GridLayout(cols=2)
+        layout.add_widget(Label(text=wallet.list_view(wallet.account_list)))
+        layout.add_widget(Button(text='World 1'))
+        layout.add_widget(Button(text='Hello 2'))
+        layout.add_widget(Button(text='World 2'))
+        return layout
 
 class SimpleApp(App):
+
     def build(self):
-        return Root()
+        return Root().layout()
+
 
 if __name__ == '__main__':
     SimpleApp().run()

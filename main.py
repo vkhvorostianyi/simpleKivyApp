fbc5cb2a11a0a5b06f690b0ad24b109f6557e840
V-Khvorostianyi <slavafh@gmail.com>
2017-12-31 08:39:53 +0200

    add buttons and else

--- main.py
+++ main.py
@@ -1,27 +1,19 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.button import Button
-from kivy.uix.label import Label
-from wallet_app import *
+
+
+class InputWindow(Widget):
+    pass
 
 
 class Root(Widget):
-
-    def layout(self):
-        layout = GridLayout(cols=2)
-        layout.add_widget(Label(text=wallet.list_view(wallet.account_list)))
-        layout.add_widget(Label(text=wallet.last_transaction_view()))
-        layout.add_widget(Button(text='Add new transaction'))
-        layout.add_widget(Button(text='View all transaction'))
-        return layout
+    pass
 
 
 class SimpleApp(App):
 
     def build(self):
-        root = Root()
-        return root.layout()
+        return Root()
 
 
 if __name__ == '__main__':

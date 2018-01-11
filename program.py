f4e34503e095938796713857fb264fe818df2a12
V-Khvorostianyi <slavafh@gmail.com>
2018-01-11 08:28:12 +0200

    add new clases and btns

--- program.py
+++ program.py
@@ -1,12 +1,20 @@
 # from kivy.uix.boxlayout import BoxLayout
 # from kivy.uix.gridlayout import GridLayout
-# from kivy.uix.widget import Widget
+from kivy.uix.widget import Widget
 # from kivy.properties import ObjectProperty
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 
 class HeadTitle(Screen):
+    pass
+
+
+class MyBtn(Screen):
+    pass
+
+
+class TransactionScreen(Screen):
     pass
 
 

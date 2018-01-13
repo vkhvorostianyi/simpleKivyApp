73c54139815d8272b49db06efe362c5695b5eaf2
V-Khvorostianyi <slavafh@gmail.com>
2018-01-13 21:35:32 +0200

    some additions

--- program.py
+++ program.py
@@ -1,17 +1,9 @@
-from kivy.uix.boxlayout import BoxLayout
-# from kivy.uix.gridlayout import GridLayout
-from kivy.uix.widget import Widget
-# from kivy.properties import ObjectProperty
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.listview import ListView
-import wallet_app as mw
 from kivy.uix.button import Button, Label
 
 
-list_item_args_converter = lambda row_index, obj: {'text': obj,
-                                                   'size_hint_y': None,
-                                                   'height': 110}
+# list_item_args_converter = lambda row_index, obj: {'text': obj, 'size_hint_y': None, 'height': 210}
 
 
 class Btn(Button):
@@ -40,9 +32,7 @@ class InputWindow(Screen):
 
 class Root(ScreenManager):
     pass
-    # start_screen = ObjectProperty()
-    # transaction_window = ObjectProperty()
-    # head_title = ObjectProperty()
+
 
 
 class StartScreen(Screen):

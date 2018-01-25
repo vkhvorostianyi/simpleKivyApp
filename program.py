84de1e5920cdf627c587fd66046ffaa48d8d6a76
V-Khvorostianyi <slavafh@gmail.com>
2018-01-25 21:06:05 +0200

    add dropdown

--- program.py
+++ program.py
@@ -1,11 +1,12 @@
-from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.label import Label
-from kivy.uix.dropdown import DropDown
-from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ListProperty, ObjectProperty
-
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.dropdown import DropDown
+from kivy.uix.gridlayout import GridLayout
+from wallet_app import  *
 
 class Btn(Screen):
     pass
@@ -49,6 +50,29 @@ class CustomDropDown(BoxLayout):
             btn.bind(on_release=lambda btn: self.dropdown.select(btn.text))
             self.dropdown.add_widget(btn)
 
+class MyGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super(MyGrid, self).__init__(**kwargs)
+        self.redraw()
+
+    sel = ["{}:{}".format(x, y) for x, y in wallet.account_list.items()]
+
+    def redraw(self):
+
+        self.clear_widgets()
+        self.rows = 5
+        self.cols =2
+
+        drpName = []
+        drpName.append(DropDown())
+        btnName = Button(text="B", size_hint=(None, None))
+        for i in self.sel:
+            btn=Button(text=i, size_hint_y=None, height=btnName.height)
+            btn.bind(on_release=lambda btn=btn,dropdown=drpName[0]:dropdown.select(btn.text))
+            drpName[0].add_widget(btn)
+        btnName.bind(on_release=drpName[0].open)
+        drpName[0].bind(on_select=lambda instance, x, btn=btnName: setattr(btn, 'text', x))
+        self.add_widget(btnName)
 
 class SimpleApp(App):
 
--- tester.py
+++ tester.py
@@ -1,17 +1,39 @@
-from kivy.uix.dropdown import DropDown
+from kivy.app import App
+from kivy.uix.label import Label
 from kivy.uix.button import Button
-from kivy.base import runTouchApp
-from wallet_app import *
+from kivy.uix.dropdown import DropDown
+from kivy.uix.gridlayout import GridLayout
+from wallet_app import  *
 
-dropdown = DropDown()
-data=["{}:{}".format(x,y) for x,y in wallet.account_list.items()]
-for index in data:
 
-    btn = Button(text=index, size_hint_y=None, height=44)
-    btn.bind(on_release=lambda btn: dropdown.select(btn.text))
-    dropdown.add_widget(btn)
 
-mainbutton = Button(text='Choose account', size_hint=(None, None))
-mainbutton.bind(on_release=dropdown.open)
-dropdown.bind(on_select=lambda instance, x: setattr(mainbutton, 'text', x))
+class MyGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super(MyGrid, self).__init__(**kwargs)
+        self.redraw()
 
+    sel = ["{}:{}".format(x, y) for x, y in wallet.account_list.items()]
+
+    def redraw(self):
+
+        self.clear_widgets()
+        self.rows = 5
+        self.cols =2
+
+        drpName = []
+        drpName.append(DropDown())
+        btnName = Button(text="B", size_hint=(None, None))
+        for i in self.sel:
+            btn=Button(text=i, size_hint_y=None, height=btnName.height)
+            btn.bind(on_release=lambda btn=btn,dropdown=drpName[0]:dropdown.select(btn.text))
+            drpName[0].add_widget(btn)
+        btnName.bind(on_release=drpName[0].open)
+        drpName[0].bind(on_select=lambda instance, x, btn=btnName: setattr(btn, 'text', x))
+        self.add_widget(btnName)
+
+class testApp(App):
+    def build(self):
+        return MyGrid()
+
+if __name__=="__main__":
+    testApp().run()

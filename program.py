6e8e88b1a6c794b3384b7891a3d2916b54b84ebf
V-Khvorostianyi <slavafh@gmail.com>
2018-02-10 10:43:05 +0200

    hard commit edit add_btn for category and account, add delete btn, edit main screen

--- program.py
+++ program.py
@@ -45,8 +45,14 @@ class CatAdd(BoxLayout):
     def __init__(self):
         super(CatAdd, self).__init__()
         self.orientation = 'vertical'
-        self.size_hint  = .5,.5
+        self.size_hint  = .75,.25
 
+
+class AccountAdd(BoxLayout):
+    def __init__(self):
+        super(AccountAdd,self).__init__()
+        self.orientation = 'vertical'
+        self.size_hint = .75, .25
 
 class MyDrop(BoxLayout):
     def __init__(self, **kwargs):
@@ -55,11 +61,14 @@ class MyDrop(BoxLayout):
 
     sel = ["{}:{}".format(x, y) for x, y in wallet.account_list.items()]
     cat = ["{}".format(x) for x in wallet.category_list.keys()]
-    def clear(self,e=None):
+    def call_cat_add_btn(self,e=None):
         self.parent.parent.add_widget(CatAdd())
         # self.remove_widget(self)
         # self.parent.parent.parent.manager.current = 'add_cat'
-
+    def call_acc_add_btn(self,e=None):
+        self.parent.parent.add_widget(AccountAdd())
+        # self.remove_widget(self)
+        # self.parent.parent.parent.manager.current = 'add_cat'
 
     def redraw(self):
 
@@ -78,9 +87,9 @@ class MyDrop(BoxLayout):
             btn.bind(on_release=lambda btn=btn,dropdown=drpName[0]:dropdown.select(btn.text))
             drpName[0].add_widget(btn)
         else:
-            btn = Button(text='add account', size_hint_y=None, height=BtnCat.height)
+            btn = Button(text='add...', size_hint_y=None, height=BtnCat.height)
             btn.bind(on_release=lambda btn=btn, dropdown=drpName[0]: dropdown.select(btn.text))
-            btn.bind(on_press=self.clear)
+            btn.bind(on_press=self.call_acc_add_btn)
             drpName[0].add_widget(btn)
         btnName.bind(on_release=drpName[0].open)
         drpName[0].bind(on_select=lambda instance, x, btn=btnName: setattr(btn, 'text', x))
@@ -91,10 +100,14 @@ class MyDrop(BoxLayout):
             btn.bind(on_release=lambda btn=btn,dropdown=drpName[1]:dropdown.select(btn.text))
             drpName[1].add_widget(btn)
         else:
-            btn = Button(text='add category', size_hint_y=None, height=BtnCat.height)
+            btn = Button(text='add...', size_hint_y=None, height=BtnCat.height)
             btn.bind(on_release = lambda btn=btn,dropdown=drpName[1]:dropdown.select(btn.text))
-            btn.bind(on_press = self.clear)
+            btn.bind(on_press = self.call_cat_add_btn)
             drpName[1].add_widget(btn)
+            del_btn = Button(text='delete...', size_hint_y=None, height=BtnCat.height)
+            del_btn.bind(on_release=lambda btn=btn, dropdown=drpName[1]: dropdown.select(btn.text))
+            del_btn.bind(on_press=self.call_cat_add_btn)
+            drpName[1].add_widget(del_btn)
         BtnCat.bind(on_release=drpName[1].open)
         drpName[1].bind(on_select=lambda instance, x, btn=BtnCat: setattr(btn, 'text', x))
         self.add_widget(BtnCat)
@@ -105,10 +118,10 @@ class SimpleApp(App):
     def build(self):
         app = Root()
         app.add_widget(StartScreen(name='main'))
-        app.add_widget(InputWindow(name="add_tr"))
-        app.add_widget(TransactionScreen(name='tr_screen'))
-        app.add_widget(CategoryScreen(name='add_cat'))
-        app.add_widget(AccountScreen(name='add_acc'))
+        app.add_widget(InputWindow(name="add_out_tr"))
+        app.add_widget(TransactionScreen(name='out_tr_screen'))
+        app.add_widget(CategoryScreen(name='in_tr_screen'))
+        app.add_widget(AccountScreen(name='add_in_tr'))
         return app
 
 

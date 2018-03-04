c16005cd81c9ec360f5302cf3d5e0392519bb410
V-Khvorostianyi <slavafh@gmail.com>
2018-03-04 20:13:50 +0200

    try profile use

--- try_profile.py
+++ try_profile.py
@@ -1,17 +1,22 @@
-def custom_profiler():
-    import cProfile, pstats, io
-    pr = cProfile.Profile()
-    pr.enable()
-    # ...
+def custom_profiler(func):
+    def wrapper():
+        import cProfile, pstats, io
+        pr = cProfile.Profile()
+        pr.enable()
+        #...do sth...
+        func()
+        pr.disable()
+        s = io.StringIO()
+        sortby = 'cumulative'
+        ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+        ps.print_stats()
+        print(s.getvalue())
+    return wrapper
 
-    pr.disable()
-    s = io.StringIO()
-    sortby = 'cumulative'
-    ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-    ps.print_stats()
-    print(s.getvalue())
 
-
-
-custom_profiler()
+@custom_profiler
+def g():
+    for i in range(10000):
+        i+=i**i
 # print(custom_profiler)
+g()

5a121701eff9982620e06c0a6ad8cdb48d271a1c
V-Khvorostianyi <slavafh@gmail.com>
2018-03-04 11:07:55 +0200

    try to make decor

--- try_profile.py
+++ try_profile.py
@@ -1,18 +1,17 @@
-import cProfile, pstats, io
+def custom_profiler():
+    import cProfile, pstats, io
+    pr = cProfile.Profile()
+    pr.enable()
+    # ...
 
-def a():
-    for i in range(100000):
-        i +=i
+    pr.disable()
+    s = io.StringIO()
+    sortby = 'cumulative'
+    ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+    ps.print_stats()
+    print(s.getvalue())
 
-pr = cProfile.Profile()
-pr.enable()
-# ... do something ...
 
-a()
-pr.disable()
-s = io.StringIO()
-sortby = 'cumulative'
-ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-ps.print_stats()
-print(s.getvalue())
 
+custom_profiler()
+# print(custom_profiler)
